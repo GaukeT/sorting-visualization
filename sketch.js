08826bf0c9932a7fd95a3ef5ab0e61b58bbb2022
Gauke Teijema <gauke@SchoolMaster.nl>
2021-05-29 14:48:02 +0200

    initial commit

--- sketch.js
+++ sketch.js
@@ -4,18 +4,14 @@ let StrokeWeight = 15;
 
 let button;
 let sort;
-let curr = -1;
+let curr;
 
 function setup() {
   createCanvas(600, 300);
   colorMode(HSB);
-  
-  sort = function() {};
-  
   resetButton();
   bubbleSortButton();
   createUnsortedArray();
-  
 }
 
 function draw() {
@@ -27,12 +23,11 @@ function draw() {
     
     line(i*StrokeWeight+StrokeWeight/2, height, i*StrokeWeight+StrokeWeight/2, height - arr[i]);
   }
-  
-  sort();
+  if (sort) sort();
 }
 
 function createUnsortedArray() {
-  sort = function() {}
+  sort = undefined;
   curr = -1;
   
   strokeWeight(StrokeWeight);
@@ -43,51 +38,52 @@ function createUnsortedArray() {
   }
 }
 
-function bubbleSort() {
-  if (arr[curr] > arr[curr+1]) {
-    let copy = arr[curr];
-    arr[curr] = arr[curr+1];
-    arr[curr+1] = copy;
-  }
-  
-  curr++;
-  
-  if (curr >= arrSize) {
-    arrSize--;
-    if (arrSize !== 0) curr = 0;
-    else curr = -1;
-  }
-  
-  if (arrSize < 0) {
-    noLoop();
-    noStroke();
-    textSize(32);
-    text('Sorted!', 10, 30);
-  }
+function resetButton() {
+  button = createButton('Reset');
+  button.position(10, 320);
+  button.mousePressed(createUnsortedArray);
+
+  button.style('border', 'none');
+  button.style('padding', '6px 10px');
+  button.style('border-radius', '6px');
+  button.style('transition-duration', '0.4s');
+}
+
+function bubbleSortButton() {
+  button = createButton('Bubble Sort');
+  button.position(80, 320);
+  button.mousePressed(startBubbleSort);
+
+  button.style('border', 'none');
+  button.style('padding', '6px 10px');
+  button.style('border-radius', '6px');
+  button.style('transition-duration', '0.4s');
+
 }
 
 function startBubbleSort() {
   sort = bubbleSort;
 }
 
-function resetButton() {
-  button = createButton('Reset');
-  button.position(10, 320);
-  
-  button.mousePressed(createUnsortedArray);
-  button.style('border', 'none');
-  button.style('padding', '6px 10px');
-  button.style('border-radius', '6px');
-  button.style('transition-duration', '0.4s');		
-}
+function bubbleSort() {
+  if (arr[curr] > arr[curr+1]) {
+    let copy = arr[curr];
+    arr[curr] = arr[curr+1];
+    arr[curr+1] = copy;
+  }
 
-function bubbleSortButton() {
-  button = createButton('Bubble Sort');
-  button.position(80, 320);
-  
-  button.mousePressed(startBubbleSort);
-  button.style('border', 'none');
-  button.style('padding', '6px 10px');
-  button.style('border-radius', '6px');
-  button.style('transition-duration', '0.4s');		
+  curr++;
+
+  if (curr >= arrSize) {
+    arrSize--;
+    if (arrSize !== 0) curr = 0;
+    else curr = -1;
+  }
+
+  if (arrSize < 0) {
+    noLoop();
+    noStroke();
+    textSize(32);
+    text('Sorted!', 10, 30);
+  }
 }
